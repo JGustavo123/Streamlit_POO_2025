33db493acb2cd05e4c8d6384de29c1e742537996
JGustavo123 <161068954+JGustavo123@users.noreply.github.com>
2025-09-23 01:51:45 +0000

    Agora foigit add .!

--- views.py
+++ views.py
@@ -1,26 +1,35 @@
 from models.cliente import Cliente, ClienteDAO
 from models.servico import servico, ServicoDAO
+
 class View:
+
     def cliente_listar():
         return ClienteDAO.listar()
-    def cliente_inserir(nome, email, fone):
-        cliente = Cliente(0, nome, email, fone)
-        ClienteDAO.inserir(cliente)
-    def cliente_atualizar(id, nome, email, fone):
-        cliente = Cliente(id, nome, email, fone)
-        ClienteDAO.atualizar(cliente)
-    def cliente_excluir(id):
-        cliente = Cliente(id, "", "", "")
-        ClienteDAO.excluir(cliente)
     
+    def cliente_inserir(nome, email, fone):
+        cliente_obj = Cliente(0, nome, email, fone)
+        ClienteDAO.inserir(cliente_obj)
+    
+    def cliente_atualizar(id, nome, email, fone):
+        cliente_obj = Cliente(id, nome, email, fone)
+        ClienteDAO.atualizar(cliente_obj)
+    
+    def cliente_excluir(id):
+        cliente_obj = Cliente(id, "", "", "")
+        ClienteDAO.excluir(cliente_obj)
+    
+
     def servico_listar():
         return ServicoDAO.listar()
+    
     def servico_inserir(descricao, valor):
-        servico = servico(0, descricao, valor)
-        ServicoDAO.inserir(servico)
+        servico_obj = servico(0, descricao, valor)  # variável renomeada
+        ServicoDAO.inserir(servico_obj)
+    
     def servico_atualizar(id, descricao, valor):
-        servico = servico(id, descricao, valor)
-        ServicoDAO.atualizar(servico)
+        servico_obj = servico(id, descricao, valor)  # variável renomeada
+        ServicoDAO.atualizar(servico_obj)
+    
     def servico_excluir(id):
-        servico = servico(id, "", "", "")
-        ServicoDAO.excluir(servico)
+        servico_obj = servico(id, "", 0)  # variável renomeada
+        ServicoDAO.excluir(servico_obj)
