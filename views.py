be5010aba15cd987835a3a4885f75ad5b86b583b
JGustavo123 <161068954+JGustavo123@users.noreply.github.com>
2025-09-23 14:55:24 +0000

    vou terminar depois

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from models.cliente import Cliente, ClienteDAO
 from models.servico import servico, ServicoDAO
+from models.horario import Horario, HorarioDAO
 
 class View:
 
@@ -33,3 +34,25 @@ class View:
     def servico_excluir(id):
         servico_obj = servico(id, "", 0)  # variável renomeada
         ServicoDAO.excluir(servico_obj)
+
+
+    def horario_inserir(data, confirmado, id_cliente, id_servico):
+        c = Horario(0, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.inserir(c)
+
+    def horario_listar():
+        return HorarioDAO.listar()
+    
+    def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
+        c = Horario(id, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.atualizar(c)
+
+    def horario_excluir(id):
+        c = Horario(id, None)
+        HorarioDAO.excluir(c)
